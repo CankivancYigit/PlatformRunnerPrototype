843a240e696c588fd2a11425b90d44fb5b7f46e5
CankivancYigit <cankivancyigit@gmail.com>
2024-10-26 08:40:48 +0300

    WIP : Player Controller scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalMoveSpeed = 10f; 
+    public float horizontalLeftLimit = -3f;  
+    public float horizontalRightLimit = 3f;
+    
+    private bool isDragging = false;  
+    private Vector3 lastPlayerPosition;   
+    private float lastMousePosX;
+    
+    void Start()
+    {
+        lastPlayerPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true; 
+            lastMousePosX = Input.mousePosition.x;
+        }
+
+        if (Input.GetMouseButton(0) && isDragging)
+        {
+            
+            float currentMousePosX = Input.mousePosition.x;
+            float mouseDeltaX = currentMousePosX - lastMousePosX;
+
+            // X eksenindeki hareketi hesaplayıp karakterin pozisyonuna uyguluyoruz
+            float moveX = mouseDeltaX / Screen.width * (horizontalRightLimit - horizontalLeftLimit);
+            Vector3 targetPosition = new Vector3(lastPlayerPosition.x + moveX, transform.position.y, transform.position.z);
+            
+            targetPosition.x = Mathf.Clamp(targetPosition.x, horizontalLeftLimit, horizontalRightLimit);
+            
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * horizontalMoveSpeed);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            lastPlayerPosition = transform.position;
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerRotationController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerRotationController : MonoBehaviour
+{
+    public float maxRotationAngle = 30f;
+    public float resetSpeed = 2f;       
+    public float delayBeforeReset = 1f;
+
+    private float currentRotationY = 0f; 
+    private bool isDragging;      
+    private Coroutine resetRotationCoroutine;  
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+            
+            if (resetRotationCoroutine != null)
+            {
+                StopCoroutine(resetRotationCoroutine);
+            }
+        }
+
+        if (Input.GetMouseButton(0) && isDragging)
+        {
+            float mouseDeltaX = Input.GetAxis("Mouse X");
+            currentRotationY += mouseDeltaX * maxRotationAngle;
+            currentRotationY = Mathf.Clamp(currentRotationY, -maxRotationAngle, maxRotationAngle);
+
+            // Mouse'a basılıyken, rotasyonu sıfırlamak için current Y rotasyonunu max açıya yaklaştığını check ediyoruz
+            if (Mathf.Abs(currentRotationY) - maxRotationAngle > -4)
+            {
+                resetRotationCoroutine = StartCoroutine(SmoothResetRotation(delayBeforeReset));
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            resetRotationCoroutine = StartCoroutine(SmoothResetRotation(0));
+        }
+        
+        transform.rotation = Quaternion.Euler(0f, currentRotationY, 0f);
+    }
+
+    private IEnumerator SmoothResetRotation(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        float targetRotationY = 0f;
+        while (Mathf.Abs(currentRotationY) > 0.01f)
+        {
+            currentRotationY = Mathf.Lerp(currentRotationY, targetRotationY, Time.deltaTime * resetSpeed);
+            transform.rotation = Quaternion.Euler(0f, currentRotationY, 0f);
+            yield return null;
+        }
+
+        currentRotationY = targetRotationY;
+        transform.rotation = Quaternion.Euler(0f, currentRotationY, 0f);
+    }
+}
