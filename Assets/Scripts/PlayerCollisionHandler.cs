05f55e6c681a082a9645ea24ed80d97b00f9a943
CankivancYigit <cankivancyigit@gmail.com>
2024-10-26 09:43:30 +0300

    DoTween imported and PlayerCollisionHAndler scirpt created

---
+++ Assets/Scripts/PlayerCollisionHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<ICollectible>(out ICollectible collectible))
+        {
+               
+            collectible.OnCollected();
+        }
+
+        if (other.TryGetComponent<ICollideable>(out ICollideable collideable))
+        {
+            collideable.OnCollide();
+        }
+    }
+}
