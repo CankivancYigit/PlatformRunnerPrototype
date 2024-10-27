9370009119519febb7bf0276c8195b7198f767e7
CankivancYigit <cankivancyigit@gmail.com>
2024-10-27 20:25:56 +0300

    WIP : Runner State Transition To Wall Painting

--- Assets/Scripts/States/RunnerState.cs
+++ Assets/Scripts/States/RunnerState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,21 @@ using UnityEngine;
 public class RunnerState : State
 {
     [SerializeField] private StateMachine stateMachine;
-    
+    [SerializeField] private State wallPaintingState;
+
+    private void OnEnable()
+    {
+        EventBus<PlayerReachedWallPaintingPosEvent>.AddListener(OnPlayerReachedWallPaintingPos);
+    }
+
     public override void EnterState()
     {
-        
+        EventBus<PlayerReachedWallPaintingPosEvent>.RemoveListener(OnPlayerReachedWallPaintingPos);
+    }
+
+    private void OnPlayerReachedWallPaintingPos(object sender, PlayerReachedWallPaintingPosEvent @event)
+    {
+        stateMachine.ChangeState(wallPaintingState);
     }
 
     public override void ExitState()
