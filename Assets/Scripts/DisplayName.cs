7eab6592b44c5ad89af4980409d05d9c3aecb282
CankivancYigit <cankivancyigit@gmail.com>
2024-10-31 00:13:22 +0300

    DisplayName script update

--- Assets/Scripts/DisplayName.cs
+++ Assets/Scripts/DisplayName.cs
@@ -3,13 +3,12 @@ using TMPro;
 
 public class DisplayName : MonoBehaviour
 {
-    public GameObject targetObject;
     private TextMeshPro _textMeshPro;
 
     void Start()
     {
         _textMeshPro = GetComponent<TextMeshPro>();
-        _textMeshPro.text = targetObject.name;
+        _textMeshPro.text = transform.parent.gameObject.name;
     }
 }
 
