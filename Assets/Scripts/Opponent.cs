d25a08820458b5b91f60bd766f2e0b3d5bd131f1
CankivancYigit <cankivancyigit@gmail.com>
2024-10-30 15:33:49 +0300

    WIP : Opponent

--- Assets/Scripts/Opponent.cs
+++ Assets/Scripts/Opponent.cs
@@ -1,55 +1,36 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
+
 public class Opponent : MonoBehaviour
 {
-	public float speed = 5f; 
-	public Vector3 startPosition; 
-	public LayerMask obstacleLayer; 
-	public float avoidDistance = 1.5f;
+	public Transform finishLine;
+	public float respawnDelay = 2f;
+	private Vector3 startPosition;
+	private NavMeshAgent agent;
 
-	private void Start()
+	void Start()
 	{
+		agent = GetComponent<NavMeshAgent>();
 		startPosition = transform.position;
-	}
-
-	void Update()
-	{
-		MoveForward();
-		AvoidObstacles();
-	}
-
-	void MoveForward()
-	{
-		// Rakibi ileriye hareket ettir
-		transform.Translate(Vector3.forward * speed * Time.deltaTime);
-	}
-
-	void AvoidObstacles()
-	{
-		RaycastHit hit;
-		
-		if (Physics.Raycast(transform.position, transform.forward, out hit, avoidDistance, obstacleLayer))
+        
+		if (finishLine != null)
 		{
-			Vector3 avoidDirection = Vector3.Cross(Vector3.up, hit.normal).normalized;
-			transform.Translate(avoidDirection * speed * Time.deltaTime);
+			agent.SetDestination(finishLine.position);
 		}
 	}
-
-	// void OnCollisionEnter(Collision other)
-	// {
-	// 	
-	// 	if (other.TryGetComponent(out ICollideable collideable) && !other.gameObject.CompareTag("RotatorStick"))
-	// 	{
-	// 		transform.position = startPosition;
-	// 	}
-	// }
 
 	private void OnTriggerEnter(Collider other)
 	{
-		
-		if (other.TryGetComponent(out ICollideable collideable))
+		if (other.TryGetComponent<ICollideable>(out var collideable))
 		{
-			transform.position = startPosition;
+			Invoke(nameof(Respawn), respawnDelay);
 		}
+	}
+
+	void Respawn()
+	{
+		transform.position = startPosition;
+		agent.SetDestination(finishLine.position);
 	}
 }
