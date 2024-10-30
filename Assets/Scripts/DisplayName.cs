1763e1bc1df39104c208d27132280714942af0ab
CankivancYigit <cankivancyigit@gmail.com>
2024-10-31 00:27:05 +0300

    WIP

--- Assets/Scripts/DisplayName.cs
+++ Assets/Scripts/DisplayName.cs
@@ -4,11 +4,17 @@ using TMPro;
 public class DisplayName : MonoBehaviour
 {
     private TextMeshPro _textMeshPro;
-
+    private Transform _mainCameraTransform;
     void Start()
     {
         _textMeshPro = GetComponent<TextMeshPro>();
         _textMeshPro.text = transform.parent.gameObject.name;
+        _mainCameraTransform = Camera.main.transform;
+    }
+    
+    void Update()
+    {
+        transform.rotation = _mainCameraTransform.rotation;
     }
 }
 
